f6ae143e4e8cda69dd37ef44086ab595a0d89abe
Bhargavi-urumadla <bhargaviu47@gmail.com>
2026-07-15 19:26:15 +0530

    Complete Shopping Features API Cart Wishlist Orders

--- server/server.js
+++ server/server.js
@@ -5,7 +5,9 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const productRoutes = require("./routes/productRoutes");
-
+const wishlistRoutes = require("./routes/wishlistRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+const orderRoutes = require("./routes/orderRoutes");
 dotenv.config();
 
 const app = express();
@@ -20,6 +22,9 @@ app.use(express.json());
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/wishlist", wishlistRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/orders", orderRoutes);
 // Test Route
 app.get("/", (req, res) => {
   res.send("🚀 ShopSmart AI Backend is Running");
